1730e07cde0f2426e1857cc05a715bf08458d973
mahidul-islam <mizihan84@gmail.com>
2021-05-06 11:10:56 +0600

    Updates

---
+++ precache-manifest.4aed1435080eaf4eb680bb1238953bbb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeb83f7b245e8410a7961653cf58748",
+    "url": "/portfolio/double-bubble-outline.png"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/portfolio/favicon.ico"
+  },
+  {
+    "revision": "f045e0742939aa280c1922cd4260cbda",
+    "url": "/portfolio/first_logo.png"
+  },
+  {
+    "revision": "f5168a3acaa27ba01e9d4add6dfe4e5d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/portfolio/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "8bb3c3e3437f7876da7552ab49410f3e",
+    "url": "/portfolio/rotating_clock.gif"
+  },
+  {
+    "revision": "3715609427f4e16f78e8",
+    "url": "/portfolio/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "3715609427f4e16f78e8",
+    "url": "/portfolio/static/js/main.37156094.chunk.js"
+  },
+  {
+    "revision": "b8bab779f3111de3cde7",
+    "url": "/portfolio/static/js/runtime~main.b8bab779.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/portfolio/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
